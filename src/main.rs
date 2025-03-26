fe296cedc10e3cfd1694e77ef0402d6c4d4e799d
cotucker <klimilya005@gmail.com>
2025-03-27 00:24:30 +0300

    feat: add initial project setup with weather app UI and functionality
    This commit introduces the initial setup for a weather application using the Slint UI framework in Rust. It includes:
    - UI icons and window design in `ui/app-window.slint`
    - Weather data fetching and processing in `src/weather.rs`
    - Configuration mappings in `src/config.rs`
    - Main application logic in `src/main.rs`
    - Build configuration in `build.rs`, `Cargo.toml`, and `.gitignore`

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+mod weather;
+mod config;
+
+use std::error::Error;
+
+slint::include_modules!();
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let weather_responce = weather::get_weather().await?;
+    weather::print_weather(&weather_responce);
+
+    let ui = MainWindow::new().unwrap();
+
+    ui.window().set_position( slint::PhysicalPosition::new(0, 0));
+    let ui_clone = ui.as_weak();
+    ui.on_mouse_move(
+            move |delta_x, delta_y| {
+            let ui_clone = ui_clone.unwrap();
+            let logical_pos = ui_clone.window().position().to_logical(ui_clone.window().scale_factor());
+            ui_clone.window().set_position(slint::LogicalPosition::new(logical_pos.x + delta_x, logical_pos.y + delta_y));
+        }
+    );
+
+    ui.on_close_window(
+        move || {
+            std::process::exit(0);
+        }
+    );
+    let ui_clone = ui.as_weak();
+    ui.on_window_minimize(
+        move || {
+            let ui_clone = ui_clone.unwrap();
+            ui_clone.window().set_minimized(true);
+        }
+    );
+    let ui_clone = ui.as_weak();
+    let weather_responce_clone = weather_responce.clone();
+    ui.on_reload_window(
+        move || {
+            set_all_params(&weather_responce_clone, &ui_clone.unwrap());
+        }
+    );
+    ui.set_country(weather::get_location(&weather_responce).0.into());
+    ui.set_city(weather::get_location(&weather_responce).1.into());
+    ui.set_condition(weather::get_condition(&weather_responce).into());
+    ui.set_temperature(weather::get_temp(&weather_responce).into());
+    ui.set_date(split_date_time(&weather::get_date(&weather_responce)).0.into());
+    ui.set_time(split_date_time(&weather::get_date(&weather_responce)).1.into());
+    let color = (0,0,0);
+    ui.set_image(slint::Image::load_from_path(std::path::Path::new("ui/icons/sun.png")).unwrap());
+    ui.set_text_color(slint::Color::from_rgb_u8(color.0, color.1, color.2));
+
+    ui.run()?;
+    Ok(())
+}
+
+fn split_date_time(date_time: &String) -> (String, String) {
+    let vec: Vec<&str> = date_time.split(' ').collect();
+    (format!("{} {} {}", vec[0], vec[1], vec[2]), format!("{} {} {}", vec[3], vec[4], vec[5]))
+}
+
+fn set_all_params(weather_responce: &weather::WeatherResponse, ui: &MainWindow) {
+    ui.set_country(weather::get_location(&weather_responce).0.into());
+    ui.set_city(weather::get_location(&weather_responce).1.into());
+    ui.set_condition(weather::get_condition(&weather_responce).into());
+    ui.set_temperature(weather::get_temp(&weather_responce).into());
+    ui.set_date(split_date_time(&weather::get_date(&weather_responce)).0.into());
+    ui.set_time(split_date_time(&weather::get_date(&weather_responce)).1.into());
+    let color = (0,0,0);
+    ui.set_image(slint::Image::load_from_path(std::path::Path::new("ui/icons/sun.png")).unwrap());
+    ui.set_text_color(slint::Color::from_rgb_u8(color.0, color.1, color.2));
+
+}
---
+++ src/weather.rs
@@ -0,0 +1,82 @@
+use reqwest::Error;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Clone)]
+pub(crate) struct WeatherResponse {
+    location: Location,
+    current: Current,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+struct Location {
+    name: String,
+    country: String,
+    localtime: String,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+struct Current {
+    temp_c: f64,
+    condition: Condition,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+struct Condition {
+    text: String,
+    code: i32,
+}
+
+pub(crate) async fn get_weather() -> Result<WeatherResponse, Error> {
+    let response = match reqwest::get("http://api.weatherapi.com/v1/current.json?key=e756ac9e718447a0ae9133510252303&q=Minsk&aqi=no")
+    .await {
+        Ok(response) => response,
+        Err(_) => panic!("Не удалось получить данные о погоде"),
+    };
+    let header = &response.headers().clone();
+    let mut response = match response.json::<WeatherResponse>().await{
+        Ok(response) => response,
+        Err(_) => get_error(),
+    };
+    println!("{:?}", header);
+    response.location.localtime = String::from(header.get("date").unwrap().to_str().unwrap());
+    Ok(response)
+}
+pub(crate) fn get_location(weather: &WeatherResponse) -> (String, String)  {
+    (weather.location.name.clone(), weather.location.country.clone())
+
+}
+pub(crate) fn get_temp(weather: &WeatherResponse) -> String {
+    format!("{}", weather.current.temp_c)
+}
+pub(crate) fn get_condition(weather: &WeatherResponse) -> String {
+    format!("{}", weather.current.condition.text)
+}
+pub(crate) fn get_date(weather: &WeatherResponse) -> String {
+    format!("{}", weather.location.localtime)
+}
+pub(crate) fn get_code(weather: &WeatherResponse) -> String {
+    format!("{}", weather.current.condition.code)
+}
+pub(crate) fn print_weather(weather: &WeatherResponse) {
+    println!("Город: {}, {}", weather.location.name, weather.location.country);
+    println!("Температура: {}°C", weather.current.temp_c);
+    println!("Состояние: {}", weather.current.condition.text);
+    println!("Время: {}", weather.location.localtime);
+}
+
+fn get_error() -> WeatherResponse {
+    WeatherResponse {
+        location: Location {
+            name: "Error".to_string(),
+            country: "Error".to_string(),
+            localtime: "Error".to_string(),
+        },
+        current: Current {
+           condition: Condition {
+            text: "Error".to_string(),
+            code: 0,
+            },
+           temp_c: 0.0,
+        },
+    }
+}
